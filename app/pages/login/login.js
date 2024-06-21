f1eab9e17fa394be405f387b37c3c9290c7b6723
lucasgfaj <lucasgfajardo09@gmail.com>
2024-06-21 20:23:08 -0300

    Edit: login.js

--- app/pages/login/login.js
+++ app/pages/login/login.js
@@ -1,31 +1,38 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // Lidar com o envio do formulário
-    $('#loginForm').on('submit', function (event) {
+    const loginForm = document.getElementById('loginForm');
+
+    loginForm.addEventListener('submit', function (event) {
         event.preventDefault(); // Impedir o envio padrão do formulário
-        const email = $('#email').val().trim(); // Remover espaços em branco extras do email
-        const senha = $('#senha').val();
+        const email = document.getElementById('email').value.trim();
+        const senha = document.getElementById('senha').value;
+
         // Verificar se os campos de e-mail e senha estão vazios
         if (!email || !senha) {
             alert('Por favor, preencha todos os campos.');
             return;
         }
-        // Obter os dados dos usuários do arquivo JSON
-        $.getJSON('/server/db.json', function (data) {
-            const cadastros = data.cadastros;
-            // Verificar se o usuário existe com o email e senha fornecidos
-            const usuario = cadastros.find(function (user) {
-                return user.email === email && user.senha === senha;
+
+        fetch('http://localhost:3000/cadastros')
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Erro ao carregar dados de usuário');
+                }
+                return response.json();
+            })
+            .then(data => {
+                console.log('Dados recebidos:', data);
+                const cadastros = data;
+                const usuario = cadastros.find(user => user.email === email && user.senha === senha);
+
+                if (usuario) {
+                    window.location.href = '/app/index.html';
+                } else {
+                    alert('E-mail ou senha inválidos. Por favor, tente novamente.');
+                }
+            })
+            .catch(error => {
+                console.error('Erro na requisição fetch:', error.message);
+                alert('Erro ao carregar dados de usuário. Por favor, tente novamente mais tarde.');
             });
-            if (usuario) {
-                // Login bem-sucedido, redirecionar para a página principal
-                window.location.href = '/app/index.html';
-            } else {
-                // Exibir mensagem de erro de login
-                alert('E-mail ou senha inválidos. Por favor, tente novamente.');
-            }
-        }).fail(function() {
-            // Exibir mensagem de erro se houver falha ao carregar o arquivo JSON
-            alert('Erro ao carregar dados de usuário. Por favor, tente novamente mais tarde.');
-        });
     });
 });
